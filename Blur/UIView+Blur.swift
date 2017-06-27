a2334bdf0ca0a6762bf9c60a14282319d3fa9a64
Roman Sorochak <roman.sorochak@gmail.com>
2017-06-27 18:45:57 +0300

    - refactor - added ability to show different use cases

--- Blur/UIView+Blur.swift
+++ Blur/UIView+Blur.swift
@@ -10,48 +10,143 @@ import UIKit
 
 extension UIView {
     
-    func applyBlurEffect(style: UIBlurEffectStyle,
-                         enableVibrancy: Bool,
-                         blurAlpha: CGFloat) {
-        self.backgroundColor = UIColor.clear
-        
-        let blurEffectView: UIVisualEffectView = {
-            let blurEffect = UIBlurEffect(style: style)
-            let blurEffectView = UIVisualEffectView(effect: blurEffect)
-            blurEffectView.translatesAutoresizingMaskIntoConstraints = false
-            
-            guard enableVibrancy else {
-                return blurEffectView
+    private struct AssociatedKeys {
+        static var descriptiveName = "AssociatedKeys.DescriptiveName.blurView"
+    }
+    
+    private (set) var blurView: BlurView {
+        get {
+            if let blurView = objc_getAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName
+                ) as? BlurView {
+                return blurView
             }
-            
-            let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
-            let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
-            blurEffectView.contentView.addSubview(vibrancyView)
-            
-            return blurEffectView
-        }()
-        
-        blurEffectView.alpha = blurAlpha
-        
-        self.insertSubview(blurEffectView, at: 0)
-        
-        func addAlignConstraintToSuperview(attribute: NSLayoutAttribute) {
-            addConstraint(
-                NSLayoutConstraint(
-                    item: blurEffectView,
-                    attribute: attribute,
-                    relatedBy: NSLayoutRelation.equal,
-                    toItem: self,
-                    attribute: attribute,
-                    multiplier: 1,
-                    constant: 0
-                )
+            self.blurView = BlurView(to: self)
+            return self.blurView
+        }
+        set(blurView) {
+            objc_setAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName,
+                blurView,
+                .OBJC_ASSOCIATION_RETAIN_NONATOMIC
             )
         }
-        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.top)
-        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.leading)
-        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.trailing)
-        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.bottom)
+    }
+    
+    class BlurView {
+        
+        private (set) var superview: UIView
+        private (set) var blur: UIVisualEffectView?
+        private var editing: Bool = false
+        
+        var animationDuration: TimeInterval = 0.1
+        
+        var style: UIBlurEffectStyle = .light {
+            didSet {
+                guard oldValue != style,
+                    !editing else { return }
+                applyBlurEffect()
+            }
+        }
+        var vibrancy: Bool = true {
+            didSet {
+                guard oldValue != vibrancy,
+                    !editing else { return }
+                applyBlurEffect()
+            }
+        }
+        var alpha: CGFloat = 0 {
+            didSet {
+                guard !editing else { return }
+                if blur == nil {
+                    applyBlurEffect()
+                }
+                let alpha = self.alpha
+                UIView.animate(withDuration: animationDuration) {
+                    self.blur?.alpha = alpha
+                }
+            }
+        }
+        
+        init(to view: UIView) {
+            self.superview = view
+        }
+        
+        func setup(style: UIBlurEffectStyle, vibrancy: Bool, alpha: CGFloat) -> Self {
+            self.editing = true
+            
+            self.style = style
+            self.vibrancy = vibrancy
+            self.alpha = alpha
+            
+            self.editing = false
+            
+            return self
+        }
+        
+        func enable(isHidden: Bool = false) {
+            if blur == nil {
+                applyBlurEffect()
+            }
+            
+            self.blur?.isHidden = isHidden
+        }
+        
+        private func applyBlurEffect() {
+            blur?.removeFromSuperview()
+            blur = applyBlurEffect(
+                style: style,
+                vibrancy: vibrancy,
+                blurAlpha: alpha
+            )
+        }
+        
+        private func applyBlurEffect(style: UIBlurEffectStyle,
+                                     vibrancy: Bool,
+                                     blurAlpha: CGFloat) -> UIVisualEffectView {
+            superview.backgroundColor = UIColor.clear
+            
+            let blurEffectView: UIVisualEffectView = {
+                let blurEffect = UIBlurEffect(style: style)
+                let blurEffectView = UIVisualEffectView(effect: blurEffect)
+                blurEffectView.translatesAutoresizingMaskIntoConstraints = false
+                
+                guard vibrancy else {
+                    return blurEffectView
+                }
+                
+                let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+                let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+                blurEffectView.contentView.addSubview(vibrancyView)
+                
+                return blurEffectView
+            }()
+            
+            blurEffectView.alpha = blurAlpha
+            
+            superview.insertSubview(blurEffectView, at: 0)
+            
+            func addAlignConstraintToSuperview(attribute: NSLayoutAttribute) {
+                superview.addConstraint(
+                    NSLayoutConstraint(
+                        item: blurEffectView,
+                        attribute: attribute,
+                        relatedBy: NSLayoutRelation.equal,
+                        toItem: superview,
+                        attribute: attribute,
+                        multiplier: 1,
+                        constant: 0
+                    )
+                )
+            }
+            addAlignConstraintToSuperview(attribute: NSLayoutAttribute.top)
+            addAlignConstraintToSuperview(attribute: NSLayoutAttribute.leading)
+            addAlignConstraintToSuperview(attribute: NSLayoutAttribute.trailing)
+            addAlignConstraintToSuperview(attribute: NSLayoutAttribute.bottom)
+            
+            return blurEffectView
+        }
     }
 }
-
--- Blur/ViewController.swift
+++ Blur/ViewController.swift
@@ -11,15 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var alphaPicker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.applyBlurEffect(
-            style: UIBlurEffectStyle.extraLight,
-            enableVibrancy: true,
-            blurAlpha: 0.8
-        )
+        imageView.blurView.setup(style: UIBlurEffectStyle.light, vibrancy: true, alpha: 0.9).enable()
+        
+        alphaPicker.selectRow(0, inComponent: 0, animated: true)
+        alphaPicker.showsSelectionIndicator = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +27,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func styleDidChange(_ sender: UISegmentedControl) {
+        imageView.blurView.style = {
+            switch sender.selectedSegmentIndex {
+            case 0:
+                return .light
+            case 1:
+                return .dark
+            case 2:
+                return .extraLight
+            default:
+                return .light
+            }
+        }()
+    }
+}
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 11
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return "alpha: \((10 - row) * 10)%"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        imageView.blurView.alpha = CGFloat(10 - row) / 10
+    }
 }
 
